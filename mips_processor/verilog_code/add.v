9637939a483cff7163238aa71b29269e85b72bf2
btb004 <btb004@bucknell.edu>
2017-09-28 15:58:50 -0400

    Added module-level comments to 3 files

--- mips_processor/verilog_code/add.v
+++ mips_processor/verilog_code/add.v
@@ -1,7 +1,14 @@
+/**
+ * Takes two numbers and outputs their sum.
+ * Inputs:  left -- 32-bit addend
+ *          right -- 32-bit addend
+ * Outputs: result -- 32-bit sum of left and right
+ */
 module adder(left, right, result);
 input [31:0] left;
 input [31:0] right;
 output [31:0] result;
+
 assign result = left + right;
 
 endmodule
--- mips_processor/verilog_code/data_memory.v
+++ mips_processor/verilog_code/data_memory.v
@@ -1,4 +1,12 @@
+/**
+ * The segment responsible for reading from and writing to the data memory.
+ * Inputs:  addr -- 32-bit address used to access memory
+ *          wdata -- 32-bit data to write to memory
+ *          mem_write -- 1-bit control signal that is high when memory needs to be written to
+ * Outputs: rdata -- 32-bit data read from memory
+ */
 module data_memory(addr, wdata, mem_write, rdata);
+
 input [31:0] addr;
 input [31:0] wdata;
 input mem_write;
@@ -6,5 +14,17 @@ output [31:0] rdata;
 
 reg [31:0] dmem[32'h7ffffffc:32'h7fff0000];
 
-initial
+// always @(*)
+// begin
+//     if (MemRead)
+//     begin
+//         readData = mem[address];
+//     end
 
+//     if (MemWrite)
+//     begin
+//         mem[address] = writeData;
+//     end
+// end
+
+endmodule

3ddf5a575b5e6449920889189c49e519031dea9a
Daniel Vasquez <dtv002@bucknell.edu>
2017-10-10 19:27:58 -0400

    Implemented Memory module and cleaned up mem.v

--- memory/mem.v
+++ memory/mem.v
@@ -1,44 +1,47 @@
-module memory(readAddress, memInstruction);
-input [31:0] readAddress;
-output reg [31:0] memInstruction;
+`ifndef MIPS
+`include "../mips.h"
+`endif
 
-reg [31:0] mem [32'h100000:32'h101000];
-initial begin
-  $readmemh("mips_code/add_test.v"
-  /*"jump.in"*/, mem);
-end
-reg [31:0] wordAddress;
-always @(readAddress)
-begin
-  wordAddress = readAddress >> 2;
-  memInstruction = mem[wordAddress];
-  //$display("readAddress: %x", readAddress);
-end
-endmodule
+`ifndef MEMORY
+`define MEMORY
 
-module data_memory(input [31:0] address, write_data, input memwrite, memread, clk, output reg [31:0] read_data);
-  reg [31:0] mem[0:255];
-  always @(posedge clk) begin
-    if (memwrite) begin
-      mem[address] = write_data;
-    end else;
-  end
-  always @(negedge clk) begin
-    if (memread) begin
-      read_data = mem[address];
-    end
-  end
-endmodule
-
-module dataMemory(input [31:0] A, WD, WE, output reg [31:0] RD);
+module Memory(input [31:0] A, WD, WE, input CLK, output reg [31:0] RD);
   reg [31:0] stack[32'h7FFBFC:32'h7FFFFC]; // 1k Stack from 7ffffffffffffffc down
   reg [31:0] text[32'h000000:32'h000400]; // 1k text 000000 up
   reg [31:0] data[32'h000401:32'h000801]; // 1k data starting from top of text going up
   initial begin
     //$readmemh("", mem);
   end
+  // Read data on the negedge of CLK
+  always @(negedge CLK) begin
+    if (A <= 32'h7FFFFC && A >= 32'h7FFBFC) begin
+      RD <= stack[A];
+    end else if (A <= 32'h000400 && A >= 32'h000000) begin
+      RD <= text[A];
+    end else if (A <= 32'h000801 && A >= 32'h000401) begin
+      RD <= data[A];
+    end else begin
+      RD <= `undefined;
+      $display("Tried to read from unallocated address %h", A);
+    end
+  end
+  // Write data on the posedge of CLK
+  always @(posedge CLK) begin
+    if (WE) begin // MemWrite signal
+      if (A <= 32'h7FFFFC && A >= 32'h7FFBFC) begin
+        stack[A] <= WD;
+      end else if (A <= 32'h000400 && A >= 32'h000000) begin
+        text[A] <= WD;
+      end else if (A <= 32'h000801 && A >= 32'h000401) begin
+        data[A] <= WD;
+      end else begin
+        $display("Tried to write to unallocated address %h", A);
+      end
+    end // MemWrite signal if block
+  end // always block
 endmodule
 
+`endif
 // // Module representing everything between the register banks surrounding the Memory state
 // module Memory(input RegWriteM, MemtoRegM, MemWriteM, input [31:0] ALUOutM, WriteDataM, input [4:0] WriteRegM,
 //               output reg [31:0] RD, output reg MemtoRegM, RegWriteM);

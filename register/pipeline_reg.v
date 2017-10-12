420da2562531e879e259db2da6f733de7a87f089
mdrr001 <mdrr001@bucknell.edu>
2017-10-12 16:15:22 -0400

    Added 1, 4, and 5 bit pipeline registers.

--- register/pipeline_reg.v
+++ register/pipeline_reg.v
@@ -20,6 +20,57 @@ module pipeline_reg(clock, clear, new_value, curr_value);
 	
 endmodule
 
+module pipeline_reg_1bit(clock, clear, new_value, curr_value);
+	input wire clock;
+	input wire clear;
+	input wire new_value;
+	output wire curr_value;
+
+	wire [31:0] adj_new_value;
+	wire [31:0] adj_curr_value;
+
+	assign adj_new_value[0] = new_value;
+	assign adj_new_value[31:1] = 0;
+	
+	assign curr_value = adj_curr_value[0];
+
+	pipeline_reg value_reg(clock, clear, adj_new_value, adj_curr_value);
+endmodule
+
+module pipeline_reg_4bit(clock, clear, new_value, curr_value);
+	input wire clock;
+	input wire clear;
+	input wire [3:0] new_value;
+	output wire [3:0] curr_value;
+
+	wire [31:0] adj_new_value;
+	wire [31:0] adj_curr_value;
+
+	assign adj_new_value[3:0] = new_value;
+	assign adj_new_value[31:4] = 0;
+
+	assign curr_value = adj_curr_value[3:0];
+
+	pipeline_reg value_reg(clock, clear, adj_new_value, adj_curr_value);
+endmodule
+
+module pipeline_reg_5bit(clock, clear, new_value, curr_value);
+	input wire clock;
+	input wire clear;
+	input wire [4:0] new_value;
+	output wire [4:0] curr_value;
+	
+	wire [31:0] adj_new_value;
+	wire [31:0] adj_curr_value;
+
+	assign adj_new_value[4:0] = new_value;
+	assign adj_new_value[31:5] = 0;
+	
+	assign curr_value = adj_curr_value[4:0];
+
+	pipeline_reg value_reg(clock, clear, adj_new_value, adj_curr_value);
+endmodule
+
 
 `endif
 

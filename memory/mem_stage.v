6d530e77ea47aaf1881111fa837ccc38ebbcb73b
Daniel Vasquez <dtv96calibre@gmail.com>
2017-10-17 16:51:22 -0400

    Wired up mem_stage

--- memory/mem_stage.v
+++ memory/mem_stage.v
@@ -4,7 +4,24 @@
 `ifndef MEM_STAGE
 `define MEM_STAGE
 
-module mem_stage();
+module mem_stage(input CLK, RegWriteE, MemtoRegE, MemWriteE,
+  input [31:0] ALUOutE, WriteDataE,
+	input [4:0] WriteRegE,
+  output wire RegWriteM, wire MemtoRegM, RD, [31:0] ALUOutM, [4:0] WriteRegM);
+
+  // Internal wires
+  wire MemWriteM;
+  wire [31:0] WriteDataM;
+
+  // Modules
+  mem_pipeline_register memPipelineRegister(
+    // inputs
+    CLK, RegWriteE, MemtoRegE,
+    MemWriteE, ALUOutE, WriteDataE, WriteRegE,
+    // outputs
+    RegWriteM, MemtoRegM, MemWriteM, ALUOutM, WriteDataM, WriteRegM);
+
+  mem dataMemory(.A(ALUOutM), .WD(WriteDataM), .WE(MemWriteM), .CLK(CLK), .MemToRegM(MemToRegM), .RD(RD));
 
 endmodule
 
